4bb092d7d8055d7fb3a5bd4245fd85356477128c
Allen <59067634+allenbronshtein@users.noreply.github.com>
2020-07-02 00:32:14 +0300

    Add files via upload

---
+++ Map/Map.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using FlightSimulatorApp.Components;
+using System.Threading;
+namespace FlightSimulatorApp.Map
+{
+    /// <summary>
+    /// Interaction logic for Map.xaml
+    /// </summary>
+    public partial class Map : UserControl
+    {
+        ViewModel db;
+        public Map()
+        {
+            InitializeComponent();
+            db = ViewModel.getInstance();
+            DataContext = db;
+        }
+    }
+}
