f38850c6aa26ca31e9571cd4796041d063a6186c
Allen <59067634+allenbronshtein@users.noreply.github.com>
2020-07-02 00:28:47 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Configuration;
+namespace FlightSimulatorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Connector.Client.Start();
+        }
+
+    }
+}
